147ca478019aa884c6fdd32d611ca7cab7e663b1
Amorphizm <noahhaupt@icloud.com>
2024-02-18 19:03:36 -0500

    Made Player.php an abstract class.

--- Classes/Player/Human.php
+++ Classes/Player/Human.php
@@ -4,7 +4,35 @@ require_once(__DIR__ . '/Player.php');
 
 class Human extends Player
 {
+    function __construct(string $name, int $teamNum, array $position) 
+    {
+      parent::__construct($name, $teamNum, $position);
+    }
 
+    /**
+     * See if this player wants to order up the card to the dealer to declare trump.
+     * @param Card $card
+     * @param string $dealerName
+     * @param bool $isDealer
+     * 
+     * @return bool
+     */
+    public function orderUpCardCheck(Card $card, string $dealerName, bool $isDealer): bool
+    {
+        $input = null;
+        $validInput = false;
+        $validInputs = ['y', 'yes', 'n', 'no'];
+
+        while (!$validInput) {
+            echo "$this->name - Would you like " . ($isDealer ? '' : $dealerName . " ") . "to pick up the $card->type of $card->suit's?\n";
+            $input = readLine("yes or no: ");
+
+            $validInput = in_array(strtolower($input), $validInputs);
+            if (!$validInput) echo "Whoops! You entered something incorrect. Please try again!\n";
+        }
+
+        return in_array(strtolower($input), ['y', 'yes']);
+    }
 }
 
 ?>
--- Classes/Player/Player.php
+++ Classes/Player/Player.php
@@ -1,6 +1,6 @@
 <?php
 
-class Player 
+abstract class Player 
 {
     public string $name;
     public int $teamNum;
@@ -16,30 +16,7 @@ class Player
         $this->nextPlayerPosition = $this->getNextPlayerPosition($position);
     }
 
-    /**
-     * See if this player wants to order up the card to the dealer to declare trump.
-     * @param Card $card
-     * @param string $dealerName
-     * @param bool $isDealer
-     * 
-     * @return bool
-     */
-    public function orderUpCardCheck(Card $card, string $dealerName, bool $isDealer): bool
-    {
-        $input = null;
-        $validInput = false;
-        $validInputs = ['y', 'yes', 'n', 'no'];
-
-        while (!$validInput) {
-            echo "$this->name - Would you like " . ($isDealer ? '' : $dealerName . " ") . "to pick up the $card->type of $card->suit's?\n";
-            $input = readLine("yes or no: ");
-
-            $validInput = in_array(strtolower($input), $validInputs);
-            if (!$validInput) echo "Whoops! You entered something incorrect. Please try again!\n";
-        }
-
-        return in_array(strtolower($input), ['y', 'yes']);
-    }
+    abstract function orderUpCardCheck(Card $card, string $dealerName, bool $isDealer): bool;
 
     /**
      * Given an player position, who should be the next player in the iteration?
