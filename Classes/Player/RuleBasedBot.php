c982a3a8e230d7422e94b32d7e32324190af7d3b
Amorphizm <noahhaupt@icloud.com>
2024-04-12 22:27:46 -0400

    Need to figure out the logic for playing the first card.

--- Classes/Player/RuleBasedBot.php
+++ Classes/Player/RuleBasedBot.php
@@ -19,23 +19,27 @@ class RuleBasedBot extends Player
         // $trashCards Remaining cards in hand belong to their own group.
         // $partnerCard Find from $playedCards, can be null if we are playing before our partner.
 
-        // Can't follow suit
-        // Do we have trump?
-            // YES - Has a trump card been played that we can't beat?
-                // YES - throw away lowest trash card.
-                // NO - Is our partner currently winning the trick point?
-                    // YES - throw away the lowest trash card.
-                    // NO  - play a trump card that wins the current trick point.
-            // NO - throw away the lowest trash card.
+        // Are we playing the first card?
+        // YES
+            
+        // NO 
+            // Can't follow suit
+            // Do we have trump?
+                // YES - Has a trump card been played that we can't beat?
+                    // YES - throw away lowest trash card.
+                    // NO - Is our partner currently winning the trick point?
+                        // YES - throw away the lowest trash card.
+                        // NO  - play a trump card that wins the current trick point.
+                // NO - throw away the lowest trash card.
 
-        // Can follow suit
-        // Do we only have one suit card we can play?
-            // YES - play it since we have to follow suit.
-            // NO - Do we have a suit card that can win the current trick point at the moment?
-                // YES - Is our partner currently winning the trick point?
-                    // YES - throw away lowest suit card.
-                    // NO - play the highest rank suit card we have.
-                // NO - throw away lowest suit card.
+            // Can follow suit
+            // Do we only have one suit card we can play?
+                // YES - play it since we have to follow suit.
+                // NO - Do we have a suit card that can win the current trick point at the moment?
+                    // YES - Is our partner currently winning the trick point?
+                        // YES - throw away lowest suit card.
+                        // NO - play the highest rank suit card we have.
+                    // NO - throw away lowest suit card.
     } 
 
     public function selectTrump(bool $stickTheDealer): ?string
